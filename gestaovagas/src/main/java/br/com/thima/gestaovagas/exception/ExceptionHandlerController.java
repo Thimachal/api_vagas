8d2c00a896a208c6c0b912c64367f454ef32270a
Thimachal <thimachal@gmail.com>
2024-03-08 11:53:19 -0300

    exceptions

--- gestaovagas/src/main/java/br/com/thima/gestaovagas/exception/ExceptionHandlerController.java
+++ gestaovagas/src/main/java/br/com/thima/gestaovagas/exception/ExceptionHandlerController.java
@@ -1,6 +1,8 @@
 package br.com.thima.gestaovagas.exception;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,6 +28,8 @@ public class ExceptionHandlerController {
             ErrorMessageDTO error = new ErrorMessageDTO(message, err.getField());
             dto.add(error);
         });
+
+        return; new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
     }
 
 }
