6a45ca951f69b9840c7a8b827a9e8a1b53bbf698
Thimachal <thimachal@gmail.com>
2024-03-11 10:06:09 -0300

    docker

--- gestaovagas/src/main/java/br/com/thima/gestaovagas/modules/candidate/controller/CandidateController.java
+++ gestaovagas/src/main/java/br/com/thima/gestaovagas/modules/candidate/controller/CandidateController.java
@@ -1,7 +1,9 @@
 package br.com.thima.gestaovagas.modules.candidate.controller;
 
 import br.com.thima.gestaovagas.modules.candidate.CandidateEntity;
+import br.com.thima.gestaovagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
 
     @PostMapping("/")
     public void create(@Valid @RequestBody CandidateEntity candidateEntity){
